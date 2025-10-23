75445734c04713789739829b990c459fee1fcd66
Felipe210ik <163340890+Felipe210ik@users.noreply.github.com>
2025-10-23 12:16:50 -0300

    Delete src directory

--- src/components/SemaforoLuz.jsx
+++
@@ -1,7 +0,0 @@
-
-const SemaforoLuz = ({ corBase, aceso }) => {
-  const classeLuz = `semaforo-luz ${aceso ? `luz-acesa-${corBase}` : ''}`;
-  return <div className={classeLuz}></div>;
-};
-
-export default SemaforoLuz;
